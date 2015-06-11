17a9f915f7d8321a60849bb1f22281c3949ff6c1
angelobestetti <angelobestetti@gmail.com>
2015-06-11 13:34:20 -0300

    Errors

--- Consulta_Serasa_cheque.php
+++ Consulta_Serasa_cheque.php
@@ -1,6 +1,6 @@
-<?php
-//error_reporting(E_ALL);
-//ini_set("display_errors", 1);
+﻿<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
 
 require_once "sws_extensao.php";
 require_once "sws_serasa_cheque.php";
